6b8f39eb24d12838faba42536b367d3418c2dde3
nix18 <kyj20167@gmail.com>
2021-02-05 21:08:18 +0800

    20210205 submit

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ async def query(uid: int, token: str):
     if veriToken.verificationToken(uid, token) != -1:
         pass
     else:
-        return {"Error": "登陆错误"}
+        return {"Error": "登陆错误，凭据失效"}
     qd_list = []
     qd = sql.session.query(sql.qiandao).all()
     sql.session.close()
@@ -45,7 +45,7 @@ async def register(uname: str, uprofile: str, upwd: str):
     try:
         isnameexist = sql.session.query(sql.user.uname).filter(sql.user.uname == uname).first() is not None
         if isnameexist:
-            return {"Error": "Name Exists"}
+            return {"Error": "名称已存在"}
         else:
             # 密码加密处理在客户端
             new_user = sql.user(uname=uname, uprofile=uprofile, upwd=upwd)
@@ -54,33 +54,37 @@ async def register(uname: str, uprofile: str, upwd: str):
             return {"Msg": "注册成功"}
     except:
         traceback.print_exc()
-        return {"Error": "数据库错误 , 请联系: " + adminMail}
+        return {"Error": "注册失败，服务器内部错误" + " 请联系: " + adminMail}
 
 
 # 登录
 @app.post("/login")
 async def login(uname: str, upwd: str):
-    cuser = sql.session.query(sql.user).filter(sql.user.uname == uname).first()
-    # 判空
-    if cuser is None:
-        return {"Error": "登录失败"}
-    cpwd = cuser.upwd
-    cuid = cuser.uid
-    if upwd == cpwd:  # 验证账号密码
-        isexist = sql.session.query(sql.tokenList.token).filter(sql.tokenList.uid == cuid).first()
-        timenext = datetime.datetime.now() + datetime.timedelta(days=3)
-        token = hashlib.sha1(os.urandom(32)).hexdigest()
-        if isexist is None:
-            new_token = sql.tokenList(uid=cuid, token=token,
-                                      expire_time=timenext)
-            sql.session.add(new_token)
+    try:
+        cuser = sql.session.query(sql.user).filter(sql.user.uname == uname).first()
+        # 判空
+        if cuser is None:
+            return {"Error": "登录失败"}
+        cpwd = cuser.upwd
+        cuid = cuser.uid
+        if upwd == cpwd:  # 验证账号密码
+            isexist = sql.session.query(sql.tokenList.token).filter(sql.tokenList.uid == cuid).first()
+            timenext = datetime.datetime.now() + datetime.timedelta(days=3)
+            token = hashlib.sha1(os.urandom(32)).hexdigest()
+            if isexist is None:
+                new_token = sql.tokenList(uid=cuid, token=token,
+                                          expire_time=timenext)
+                sql.session.add(new_token)
+            else:
+                sql.session.query(sql.tokenList).filter(sql.tokenList.uid == cuid) \
+                    .update({"token": token, "expire_time": timenext})
+            sql.session.commit()
+            return {"Token": token}
         else:
-            sql.session.query(sql.tokenList).filter(sql.tokenList.uid == cuid) \
-                .update({"token": token, "expire_time": timenext})
-        sql.session.commit()
-        return {"Token": token}
-    else:
-        return {"Error": "登录失败"}
+            return {"Error": "登录失败"}
+    except:
+        traceback.print_exc()
+        return {"Error": "登录失败，服务器内部错误" + " 请联系: " + adminMail}
 
 
 # 签到
@@ -96,7 +100,7 @@ async def qiandao(uname: str, token: str):
         return {"Error": "签到失败，凭据出错"}
     except:
         traceback.print_exc()
-        return {"Error": "签到失败，服务器内部错误"}
+        return {"Error": "签到失败，服务器内部错误" + " 请联系: " + adminMail}
 
 
 # 添加好习惯
@@ -113,54 +117,83 @@ async def add_habit(uid: int, token: str, hname: str, hcontent: str, hcategory: 
         else:
             return {"Error": "习惯添加失败，凭据失效"}
     except:
-        return {"Error": "习惯添加失败，服务器内部错误"}
+        traceback.print_exc()
+        return {"Error": "习惯添加失败，服务器内部错误" + " 请联系: " + adminMail}
 
 
 # 查询好习惯 f'{hname}'为格式化字符串，注意引号
 @app.get("/selhabits")
 async def sel_habits(hname: str = None, hcategory: str = None):
-    if hname is None:
-        if hcategory is None:
-            habits = sql.session.query(sql.goodHabits).all()
+    try:
+        if hname is None:
+            if hcategory is None:
+                habits = sql.session.query(sql.goodHabits).all()
+            else:
+                habits = sql.session.query(sql.goodHabits).filter(sql.goodHabits.hcategory.like(f'%{hcategory}%')).all()
         else:
-            habits = sql.session.query(sql.goodHabits).filter(sql.goodHabits.hcategory.like(f'%{hcategory}%')).all()
-    else:
-        if hcategory is None:
-            habits = sql.session.query(sql.goodHabits).filter(sql.goodHabits.hname.like(f'%{hname}%')).all()
-        else:
-            habits = sql.session.query(sql.goodHabits).filter(
-                sql.goodHabits.hname.like(f'%{hname}%'), sql.goodHabits.hcategory.like(f'%{hcategory}%')).all()
-    return habits
+            if hcategory is None:
+                habits = sql.session.query(sql.goodHabits).filter(sql.goodHabits.hname.like(f'%{hname}%')).all()
+            else:
+                habits = sql.session.query(sql.goodHabits).filter(
+                    sql.goodHabits.hname.like(f'%{hname}%'), sql.goodHabits.hcategory.like(f'%{hcategory}%')).all()
+        return habits
+    except:
+        traceback.print_exc()
+        return {"Error": "查询习惯失败，服务器内部错误" + " 请联系: " + adminMail}
 
 
 @app.get("/selmyhabits")
 async def sel_my_habits(uid: int, token: str):
     cuid = veriToken.verificationToken(uid, token)
-    if cuid != -1:
-        return sql.session.query(sql.goodHabits).filter(sql.goodHabits.cuid == cuid).all()
-    return {"Error": "查询自定义习惯失败，凭据失效"}
+    try:
+        if cuid != -1:
+            return sql.session.query(sql.goodHabits).filter(sql.goodHabits.cuid == cuid).all()
+        return {"Error": "查询自定义习惯失败，凭据失效"}
+    except:
+        traceback.print_exc()
+        return {"Error": "查询自定义习惯失败，服务器内部错误" + " 请联系: " + adminMail}
 
 
 @app.get("/modhabit")
 async def mod_habit(uid: int, token: str, hid: int, hname: str = None, hcontent: str = None, hcategory: str = None):
     cuid = veriToken.verificationToken(uid, token)
-    if cuid != -1:
-        if tuple([hid]) in sql.session.query(sql.goodHabits.hid).filter(sql.goodHabits.cuid == cuid).all():
-            if hname is not None:
-                sql.session.query(sql.goodHabits).filter(sql.goodHabits.hid == hid) \
-                    .update({sql.goodHabits.hname: hname})
+    try:
+        if cuid != -1:
+            if tuple([hid]) in sql.session.query(sql.goodHabits.hid).filter(sql.goodHabits.cuid == cuid).all():
+                if hname is not None:
+                    sql.session.query(sql.goodHabits).filter(sql.goodHabits.hid == hid) \
+                        .update({sql.goodHabits.hname: hname})
+                    sql.session.commit()
+                if hcontent is not None:
+                    sql.session.query(sql.goodHabits).filter(sql.goodHabits.hid == hid) \
+                        .update({sql.goodHabits.hcontent: hcontent})
+                    sql.session.commit()
+                if hcategory is not None:
+                    sql.session.query(sql.goodHabits).filter(sql.goodHabits.hid == hid) \
+                        .update({sql.goodHabits.hcategory: hcategory})
+                    sql.session.commit()
+                return {"Msg": "修改成功"}
+            return {"Error": "修改失败，无权操作他人的习惯"}
+        return {"Error": "修改失败，凭据失效"}
+    except:
+        traceback.print_exc()
+        return {"Error": "修改失败，服务器内部错误" + " 请联系: " + adminMail}
+
+
+@app.get("/delhabit")
+async def del_habit(uid: int, token: str, hid: int):
+    cuid = veriToken.verificationToken(uid, token)
+    try:
+        if cuid != -1:
+            if tuple([hid]) in sql.session.query(sql.goodHabits.hid).filter(sql.goodHabits.cuid == cuid).all():
+                sql.session.query(sql.goodHabits).filter(sql.goodHabits.hid == hid).delete()
                 sql.session.commit()
-            if hcontent is not None:
-                sql.session.query(sql.goodHabits).filter(sql.goodHabits.hid == hid) \
-                    .update({sql.goodHabits.hcontent: hcontent})
-                sql.session.commit()
-            if hcategory is not None:
-                sql.session.query(sql.goodHabits).filter(sql.goodHabits.hid == hid) \
-                    .update({sql.goodHabits.hcategory: hcategory})
-                sql.session.commit()
-            return {"Msg": "修改成功"}
-        return {"Error": "修改失败，无权操作他人的习惯"}
-    return {"Error": "修改失败，凭据失效"}
+                return {"Msg": "删除成功"}
+            return {"Error": "删除失败，无权操作他人的习惯"}
+        return {"Error": "删除失败，凭据失效"}
+    except:
+        traceback.print_exc()
+        return {"Error": "删除失败，服务器内部错误" + " 请联系: " + adminMail}
 
 
 @app.get("/items/{item_id}")
